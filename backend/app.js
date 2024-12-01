bce8fcd7678bfa5f383adbc3c5e5254e3fa36933
ashif <hasanat153@gmail.com>
2024-12-01 23:42:03 +0600

    app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const  cors = require("cors");
 const app = express();
 // Configure CORS
 app.use(cors({
-    origin: "http://localhost:5173", // Allow requests from your frontend
+    origin: ["http://localhost:5173","https://gymsmmsystemtask.onrender.com/"], // Allow requests from your frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
    // credentials: true, // If you're using cookies or authentication
   }));
