6b458bed78bc31bc10c8d1e3ad72802236411337
ashif <hasanat153@gmail.com>
2024-12-01 22:38:26 +0600

    ..

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ const app = require("./app");
 
 
 const port = process.env.PORT || 5000;
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
-})
 
 app.listen(port, (req, res) => {
 
