308bca6df8855daf91853a32c9c19d91007f68c7
ashif <hasanat153@gmail.com>
2024-12-02 12:46:00 +0600

    ..

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, (req, res) => {
+app.listen(PORT, (req, res) => {
 
   console.log("Server now start Successfully at http://localhost:" + port);
 
